834632c06a9caea4bad4b776e1227cfe3fd06767
Ilan Filonenko <if56@cornell.edu>
2017-10-31 01:53:19 -0400

    added multiplication of types and subdomain exporting

--- src/scaled.jl
+++ src/scaled.jl
@@ -9,6 +9,7 @@ struct Scaled{T<:Signed,f,s,r <: RoundingScheme} <: ScaledFixedPoint{T,f,s,r}
     Scaled{T,f,s,r}(x) where {T,f,s,r <: RoundingScheme} =
         convert(Scaled{T,f,s,r}, x)
 end
+export sat_add, subdomain
 typechar(::Type{X}) where {X <: Scaled} = 'S'
 signbits(::Type{X}) where {X <: Scaled} = 1
 # datatype comparison
@@ -23,25 +24,25 @@ up(x::Type{T},b1::I,b2::I) where {T <: Signed,I <: Integer} = b1+b2 > maxf(x) ? 
 -(x::Scaled{T,f,s,r}) where {T,f,s,r} = Scaled{T,f,s,r}(-x.i,true) # negation
 abs(x::Scaled{T,f,s,r}) where {T,f,s,r} = Scaled{T,f,s,r}(abs(x.i),true) # absolute value
 # Saturated Add when T,f,s,r are the same
-+(x::Scaled{T,f,s,r}, y::Scaled{T,f,s,r}) where {T <:Signed,f,s,r<: RoundingScheme} =
+sat_add(x::Scaled{T,f,s,r}, y::Scaled{T,f,s,r}) where {T <:Signed,f,s,r<: RoundingScheme} =
     Scaled{T,f,s,r}(clamp(x.i+y.i,-(1<<f),(1<<f-1)))
 # Exact adds: only when f's are different
 +(x::Scaled{T1,f1,s,r}, y::Scaled{T2,f2,s,r}) where {T1,T2 <:Signed,f1,f2,s,r<: RoundingScheme} =
     Scaled{leq(T1,T2) ? up(T2,f1,f2) : up(T1,f1,f2),max(f1,f2)+1,s,r}(x.i+y.i)
 -(x::Scaled{T,f,s,r}, y::Scaled{T,f,s,r}) where {T <:Signed,f,s,r<: RoundingScheme} =
     Scaled{T,f,s,r}(clamp(x.i-y.i,-(1<<f),(1<<f-1)))
+
 # Multiplication
 *(x::Scaled{T1,f1,s1,r}, y::Scaled{T2,f2,s2,r}) where {T1,T2 <:Signed,f1,s1,f2,s2,r<: RoundingScheme} =
     Scaled{leq(T1,T2) ? up(T2,f1,f2) : up(T1,f1,f2),f1+f2,s1*s2,r}(Base.widemul(x.i,y.i))
-# Dot Product, which needs to be overloaded to ensure that bit sizes does not expotentaially increase
-dot(x::Array{Scaled{T1,f1,s1,r}}, y::Array{Scaled{T2,f2,s2,r}}) where {T1,T2 <:Signed,f1,s1,f2,s2,r<: RoundingScheme} =
-    sum(collect(map(x->float(x[1]*x[2]) , collect(zip(x,y)))))
-# Matrix Multiplication, needs to be overloaded with same reason as Dot Product
 
+# Type Multiplication
+*(x::Type{Scaled{T1,f1,s1,r}}, y::Type{Scaled{T2,f2,s2,r}}) where {T1,T2 <:Signed,f1,s1,f2,s2,r<: RoundingScheme} =
+    Scaled{leq(T1,T2) ? up(T2,f1,f2) : up(T1,f1,f2),f1+f2,s1*s2,r}
 
 # extended functions
 # Whether a Scaled is within the same subdomain as another Scaled
-subdomain(x::Scaled{T1,f1,s1,r}, y::Scaled{T2,f2,s2,r}) where {T1,T2 <:Signed,f1,s1,f2,s2,r<: RoundingScheme} =
+subdomain(x::Type{Scaled{T1,f1,s1,r}}, y::Type{Scaled{T2,f2,s2,r}}) where {T1,T2 <:Signed,f1,s1,f2,s2,r<: RoundingScheme} =
     isinteger(s1/s2) && log2(s1/s2) <= f2-f1
 # conversions
 convert(::Type{Scaled{T,f,s,r}}, x::Integer) where {T,f,s,r} = Scaled{T,f,s,r}(T(x/s),0)
