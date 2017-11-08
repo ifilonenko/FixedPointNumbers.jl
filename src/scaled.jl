3d0657b3a95fdd8d39a5a79b4c59d333974bbdc5
Ilan Filonenko <if56@cornell.edu>
2017-11-08 15:45:22 -0500

    modified subdomain

--- src/scaled.jl
+++ src/scaled.jl
@@ -21,20 +21,20 @@ leq(x::Type{T1}, y::Type{T2}) where {T1,T2 <: Signed} =  sizeof(x)<=sizeof(y)
 up(x::Type{T},b1::I,b2::I) where {T <: Signed,I <: Integer} = b1+b2 > maxf(x) ? widen1(x) : x
 
 # basic operators
--(x::Scaled{T,f,s,r}) where {T,f,s,r} = Scaled{T,f,s,r}(-x.i,true) # negation
-abs(x::Scaled{T,f,s,r}) where {T,f,s,r} = Scaled{T,f,s,r}(abs(x.i),true) # absolute value
+-(x::Scaled{T,f,s,r}) where {T,f,s,r} = Scaled{T,f,s,r}(-x.i,0) # negation
+abs(x::Scaled{T,f,s,r}) where {T,f,s,r} = Scaled{T,f,s,r}(abs(x.i),0) # absolute value
 # Saturated Add when T,f,s,r are the same
 sat_add(x::Scaled{T,f,s,r}, y::Scaled{T,f,s,r}) where {T <:Signed,f,s,r<: RoundingScheme} =
-    Scaled{T,f,s,r}(clamp(x.i+y.i,-(1<<f),(1<<f-1)))
+    Scaled{T,f,s,r}(clamp(x.i+y.i,-(1<<f),(1<<f-1)),0)
 # Exact adds: only when f's are different
 +(x::Scaled{T1,f1,s,r}, y::Scaled{T2,f2,s,r}) where {T1,T2 <:Signed,f1,f2,s,r<: RoundingScheme} =
-    Scaled{leq(T1,T2) ? up(T2,f1,f2) : up(T1,f1,f2),max(f1,f2)+1,s,r}(x.i+y.i)
+    Scaled{leq(T1,T2) ? up(T2,f1,f2) : up(T1,f1,f2),max(f1,f2)+1,s,r}(x.i+y.i,0)
 -(x::Scaled{T,f,s,r}, y::Scaled{T,f,s,r}) where {T <:Signed,f,s,r<: RoundingScheme} =
-    Scaled{T,f,s,r}(clamp(x.i-y.i,-(1<<f),(1<<f-1)))
+    Scaled{T,f,s,r}(clamp(x.i-y.i,-(1<<f),(1<<f-1)),0)
 
 # Multiplication
 *(x::Scaled{T1,f1,s1,r}, y::Scaled{T2,f2,s2,r}) where {T1,T2 <:Signed,f1,s1,f2,s2,r<: RoundingScheme} =
-    Scaled{leq(T1,T2) ? up(T2,f1,f2) : up(T1,f1,f2),f1+f2,s1*s2,r}(Base.widemul(x.i,y.i))
+    Scaled{leq(T1,T2) ? up(T2,f1,f2) : up(T1,f1,f2),f1+f2,s1*s2,r}(Base.widemul(x.i,y.i),0)
 
 # Type Multiplication
 *(x::Type{Scaled{T1,f1,s1,r}}, y::Type{Scaled{T2,f2,s2,r}}) where {T1,T2 <:Signed,f1,s1,f2,s2,r<: RoundingScheme} =
@@ -43,7 +43,8 @@ sat_add(x::Scaled{T,f,s,r}, y::Scaled{T,f,s,r}) where {T <:Signed,f,s,r<: Roundi
 # extended functions
 # Whether a Scaled is within the same subdomain as another Scaled
 subdomain(x::Type{Scaled{T1,f1,s1,r}}, y::Type{Scaled{T2,f2,s2,r}}) where {T1,T2 <:Signed,f1,s1,f2,s2,r<: RoundingScheme} =
-    isinteger(s1/s2) && log2(s1/s2) <= f2-f1
+    isinteger(s1/s2) && log2(s1/s2) <= maxf(T2)-maxf(T1)+2
+    
 # conversions
 convert(::Type{Scaled{T,f,s,r}}, x::Integer) where {T,f,s,r} = Scaled{T,f,s,r}(T(x/s),0)
 function convert(::Type{Scaled{T,f,s,r}}, x::AbstractFloat) where {T,f,s,r <: RoundingScheme}
